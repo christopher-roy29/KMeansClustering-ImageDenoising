20925d3903a1b77ecb00db407a3b632dcbba5e2d
Christopher Roy <36618302+christopher-roy29@users.noreply.github.com>
2021-03-01 19:09:04 -0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,4 @@
-"""
-K-Means Segmentation Problem
-(Due date: Nov. 25, 11:59 P.M., 2019)
-The goal of this task is to segment image using k-means clustering.
 
-Do NOT modify the code provided to you.
-Do NOT import ANY library or API besides what has been listed.
-Hint: 
-Please complete all the functions that are labeled with '#to do'. 
-You are allowed to add your own functions if needed.
-You should design you algorithm as fast as possible. To avoid repetitve calculation, you are suggested to depict clustering based on statistic histogram [0,255]. 
-You will be graded based on the total distortion, e.g., sum of distances, the less the better your clustering is.
-"""
 
 
 import utils
